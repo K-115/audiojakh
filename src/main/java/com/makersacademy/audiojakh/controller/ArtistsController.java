dccfc7d34feece82784a8793b36c850295761ae0
Jake Watson <jakelukewatson@gmail.com>
2026-06-25 15:25:48 +0100

    artist controller query and changes needed to existing files

---
+++ src/main/java/com/makersacademy/audiojakh/controller/ArtistsController.java
@@ -0,0 +1,29 @@
+package com.makersacademy.audiojakh.controller;
+
+import com.makersacademy.audiojakh.model.Artist;
+import com.makersacademy.audiojakh.model.Track;
+import com.makersacademy.audiojakh.repository.AlbumRepository;
+import com.makersacademy.audiojakh.repository.ArtistRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+public class ArtistsController {
+
+    @Autowired
+    AlbumRepository albumRepository;
+
+    @Autowired
+    ArtistRepository artistRepository;
+
+    @GetMapping("/artists/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        model.addAttribute("artist", artist);
+        model.addAttribute("albums", albumRepository.findAlbumsByArtistId(id));
+
+        return "artists/show";
+    }
+}
