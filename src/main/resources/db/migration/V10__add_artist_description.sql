da795a250e002c3e46ce6eceb324838d53c84298
Jake Watson <jakelukewatson@gmail.com>
2026-06-26 10:06:42 +0100

    remove duplicate line

--- src/main/resources/db/migration/V10__add_artist_description.sql
+++ src/main/resources/db/migration/V10__add_artist_description.sql
@@ -1,7 +1,5 @@
 ALTER TABLE artists ADD COLUMN IF NOT EXISTS description TEXT;
 
-ALTER TABLE artists ADD COLUMN IF NOT EXISTS description TEXT;
-
 UPDATE artists SET description = 'Pulitzer Prize-winning rapper from Compton, California, widely regarded as one of the most influential hip-hop artists of his generation.' WHERE name = 'Kendrick Lamar';
 UPDATE artists SET description = 'Canadian rapper, singer and songwriter from Toronto who became one of the best-selling music artists of the 2010s.' WHERE name = 'Drake';
 UPDATE artists SET description = 'American R&B singer-songwriter known for her genre-blending sound and the acclaimed albums Ctrl and SOS.' WHERE name = 'SZA';
